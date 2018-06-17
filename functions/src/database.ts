1650f23afebacff564fb17d5a9dc96d5d3c41bde
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-06-18 03:09:52 +0530

    Create function to execute transaction

--- functions/src/database.ts
+++ functions/src/database.ts
@@ -5,7 +5,7 @@ import { ENGINE_METHOD_PKEY_ASN1_METHS } from "constants";
 // Get key by email
 const getKey = function(email){
     return new Promise(async function(resolve, reject){
-        var key = null
+        let key = null
         await admin.database().ref("users").orderByChild("email").equalTo(email).once("value")
         .then((snapshot) => {
             snapshot.forEach(child => { key = child.key })
@@ -22,7 +22,7 @@ const getKey = function(email){
 }
 
 // Get balance
-var getBalance = function(uid){
+const getBalance = function(uid){
     return new Promise(async function(resolve, reject){
         await admin.database().ref("/users/" + uid + "/account").once('value')
         .then((snapshot) => {
@@ -35,10 +35,38 @@ var getBalance = function(uid){
 }
 
 // Add log entry
+const executeTransaction = function(type, title, description, amount, initiatorId, receiverId, created){
+    return new Promise(async function(resolve, reject){
+        const initiatorLogRef = admin.database().ref("/users/" + initiatorId + "/account/log")
+        const receiverLogRef  = admin.database().ref("/users/" + receiverId  + "/account/log")
+        
+        // Update initiator's log
+        await initiatorLogRef.push({
+            "type" : "SEND",
+            "title" : title,
+            "description" : description,
+            "amount" : amount,
+            "receiverId" : receiverId,
+            "created" : created
+        })
 
+        // Update receiver's log
+        await receiverLogRef.push({
+            "type" : "RECEIVE",
+            "title" : title,
+            "description" : description,
+            "amount" : amount,
+            "initiatorId" : initiatorId,
+            "created" : created
+        })
+
+        resolve("Transaction Complete")
+    })
+}
 
 module.exports = {
     getKey: getKey,
-    getBalance: getBalance
+    getBalance: getBalance,
+    executeTransaction: executeTransaction
 }
 
